e4d8fd34b187a44e483f3fc962a151ae0e45ae1e
Tipuch <fiorini751@gmail.com>
2021-04-01 17:47:44 -0400

    fixed static folder

---
+++ static/src/components/Video.vue
@@ -0,0 +1,21 @@
+<template>
+<div id="vs"></div>
+</template>
+
+<script>
+import Player from 'xgplayer';
+
+export default {
+  name: "Video",
+  mounted() {
+    new Player({
+        id: 'vs',
+        url: 'http://s2.pstatp.com/cdn/expire-1-M/byted-player-videos/1.0.0/xgplayer-demo.mp4'
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
